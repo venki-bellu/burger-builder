85e0536fd83b1d3feba2834abc8dc043ffa344cb
venkibellu <venkateshrbelavadi@gmail.com>
2019-10-18 19:57:24 +0530

    8.132 Adding a Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Auxi from '../../hoc/Auxi';
 import classes from './Layout.module.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Auxi>
-        <div>
-            Toolbar, SideDrawer, Backdrop
-        </div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
