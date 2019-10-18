b0a489bdf793687127701737a0a79816194369ba
venkibellu <venkateshrbelavadi@gmail.com>
2019-10-18 17:32:14 +0530

    8.126 Creating the Order Summary Modal

--- src/components/Burger/BurgerIngredients/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredient.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-typos */
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 
 import classes from './BurgerIngredient.module.css'
 
@@ -43,8 +43,8 @@ class BurgerIngredient extends Component{
     
 };
 
-BurgerIngredient.PropTypes = {
-    type: PropTypes.string.isRequired
+BurgerIngredient.propTypes = {
+    type: propTypes.string.isRequired
 };
 
 export default BurgerIngredient;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import Auxi from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrdarSummary';
 
 const INGREDIENT_PRICES = {
     salad: 15,
@@ -85,6 +87,9 @@ class BurgerBuilder extends Component
         }
         return (
             <Auxi>
+                <Modal>
+                    <OrderSummary ingredients = {this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients = {this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded ={this.addIngredientHandler}
