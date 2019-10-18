d7147728716f907d41614439d6efaffee9285894
venkibellu <venkateshrbelavadi@gmail.com>
2019-10-18 11:20:23 +0530

    8.114 Starting Implementation of The Burger Builder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Auxi from '../../hoc/Auxi';
+import classes from './Layout.module.css'
 
 const layout = (props) => (
     <Auxi>
         <div>
             Toolbar, SideDrawer, Backdrop
         </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxi>
---
+++ src/containers/BurgerBuilder/BurgetBuilder.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Auxi from '../../hoc/Auxi'
+
+class BurgerBuilder extends Component
+{
+    render()
+    {
+        return (
+            <Auxi>
+                <div>Burger</div>
+                <div>Burger Control</div>
+            </Auxi>
+        );
+    }
+}
+
+export default BurgerBuilder;
