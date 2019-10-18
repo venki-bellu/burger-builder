3cfa60f7d2130fccd6d9a1fff2463d7289a7b089
venkibellu <venkateshrbelavadi@gmail.com>
2019-10-18 22:33:53 +0530

    8.137 More about Responsive Adjustments

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Auxi from '../../hoc/Auxi';
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Auxi>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
