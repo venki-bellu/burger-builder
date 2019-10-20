661b984c2c926b80ad4acd1d6347eb6c2b12f137
venkibellu <venkateshrbelavadi@gmail.com>
2019-10-20 11:40:14 +0530

    8.143 Improving Performance

--- src/components/Burger/OrderSummary/OrdarSummary.js
+++ src/components/Burger/OrderSummary/OrdarSummary.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 import Auxi from '../../../hoc/Auxi';
 import Button from '../../UI/Button/Button';
-const orderSummary = props =>{
-    const ingredientsSummary = Object.keys(props.ingredients)
+
+class OrderSummary extends Component{
+    
+    render()
+    {
+        const ingredientsSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
         return (<li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}> {igKey} </span> : {props.ingredients[igKey]}
+                    <span style={{textTransform: 'capitalize'}}> {igKey} </span> : {this.props.ingredients[igKey]}
                 </li>)
-    });
-    return(
-        <Auxi>
-            <h3>Your order!</h3>
-            <p>A delcious burger with the following ingredients</p>
-            <ul>
-                {ingredientsSummary}
-            </ul>
-            <p><strong>Total Price: &#8377;{props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Auxi>
-    )
+        });
+        return(
+            <Auxi>
+                <h3>Your order!</h3>
+                <p>A delcious burger with the following ingredients</p>
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+                <p><strong>Total Price: &#8377;{this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Auxi>
+        )
+    }
+    
 };
 
-export default orderSummary;
+export default OrderSummary;
