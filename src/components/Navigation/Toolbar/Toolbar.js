187fd018885391319b4ad9dc47f9af048327c598
venkibellu <venkateshrbelavadi@gmail.com>
2019-10-20 11:05:58 +0530

    8.140 Adding a Hamburger Icon

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,6 +4,7 @@ import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems';
 import DrawerToggler from '../SideDrawer/DrawerToggle/DrawerToggle';
+
 const toolbar = props =>(
     <header className={classes.Toolbar}>
         <DrawerToggler clicked = {props.drawerToggleClicked}/>
